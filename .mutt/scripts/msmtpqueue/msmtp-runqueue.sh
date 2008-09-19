a45f96a81a45494ff13cf03ea1a4fd1641a06021
Angel Olivera <redondos@gmail.com>
2008-09-19 17:35:37 +0200

    daemonized msmtp

--- .mutt/scripts/msmtpqueue/msmtp-runqueue.sh
+++ .mutt/scripts/msmtpqueue/msmtp-runqueue.sh
@@ -39,7 +39,7 @@ for MAILFILE in *.mail; do
 		echo "FAILURE"
 		continue
 	fi
-	msmtp `cat "$MSMTPFILE"` < "$MAILFILE"
+	msmtp -d `cat "$MSMTPFILE"` < "$MAILFILE"
 	if [ $? -eq 0 ]; then
 		rm "$MAILFILE" "$MSMTPFILE"
 		echo "$MAILFILE sent successfully"
